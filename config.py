7f9aa58b5faa732e227b9ac7dbf21b5c70e4de56
Epic-Shubham <163536954+Epic-Shubham@users.noreply.github.com>
2024-05-01 16:43:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,98 @@
+import re, os, time
+id_pattern = re.compile(r'^.\d+$') 
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","madflixbot")     
+    DB_URL  = os.environ.get("DB_URL","")
+ 
+    # other configs
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "https://te.legra.ph/file/a286701a4edd463286a07.png")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
+
+
+class Txt(object):
+    # part of text configuration
+        
+    START_TXT = """Hello {} 
+    
+➻ This Is An Advanced And Yet Powerful Rename Bot.
+    
+➻ Using This Bot You Can Auto Rename Of Your Files.
+    
+➻ This Bot Also Supports Custom Thumbnail And Custom Caption.
+    
+➻ Use /tutorial Command To Know How To Use Me.
+    
+<b>Bot Is Made By Big Brain</b>"""
+    
+    FILE_NAME_TXT = """<b><u>SETUP AUTO RENAME FORMAT</u></b>
+
+Use These Keywords To Setup Custom File Name
+
+✓ episode :- To Replace Episode Number
+✓ quality :- To Replace Video Resolution
+
+<b>➻ Example :</b> <code> /autorename Naruto Shippuden S02 - EPepisode - quality  [Dual Audio] - @anime_bash </code>
+
+<b>➻ Your Current Auto Rename Format :</b> <code>{format_template}</code> """
+    
+    ABOUT_TXT = f"""<b>🤖 My Name :</b> <a href='BigBrain'>Auto Rename Bot ⚡</a>
+<b>📢 Channel :</b> <a href='https://t.me/anime_bash_offical'>AnimeBash Botz</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/anime_bash_chat'>Big Brain Developer</a>
+    
+<b>♻️ Bot Made By :</b> Big_Brain"""
+
+    
+    THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
+    
+⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....
+    
+⦿ /viewthumb - Use This Command To See Your Thumbnail
+⦿ /delthumb - Use This Command To Delete Your Thumbnail"""
+
+    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+    
+⦿ /set_caption - Use This Command To Set Your Caption
+⦿ /see_caption - Use This Command To See Your Caption
+⦿ /del_caption - Use This Command To Delete Your Caption"""
+
+   PROGRESS_BAR = """\n
+<b>┌────────────────────── Progress ──────────────────────┐
+<b>│ Size    : {1} | {2}
+<b>│ Done    : {0}%
+<b>│ Speed   : {3}/s
+<b>│ ETA     : {4}
+<b>└─────────────────────────────────────────────┘
+"""
+    
+    DONATE_TXT = """<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+    
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+    
+<b>🛍 UPI ID:</b> <code>Animebash@ybl</code> """
+    
+    HELP_TXT = """<b>Hey</b> {}
+    
+Here Is The Help For My Commands."""
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Developer @JishuDeveloper
+
